f91eb665c843448aec11961f351c7042df65cd8c
Chowdhury <md.nasif008@gmail.com>
2026-04-12 22:39:46 +0200

    ft_toupper added

---
+++ ft_toupper.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "libft.h"
+
+
+// toupper -- lower case to upper case letter conversion
+int	ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+
+// int	main()
+// {
+// 	char str[] = "Hello World!";
+// 	int i = 0;
+
+// 	printf("Before: %s\n", str);
+
+// 	while (str[i])
+// 	{
+// 		str[i] = ft_toupper(str[i]);
+// 		i++;
+// 	}
+
+// 	printf("After : %s\n", str);
+// }
