1be8ffb50b024de4e18643a953fde5e5009596b3
Chowdhury <md.nasif008@gmail.com>
2026-04-22 01:20:41 +0200

    Makefile updated

---
+++ ft_lstiter.c
@@ -0,0 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchowdhu <mchowdhu@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/22 00:51:03 by mchowdhu          #+#    #+#             */
+/*   Updated: 2026/04/22 00:51:03 by mchowdhu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Iterates through the list ’lst’ and applies the
+// function ’f’ to the content of each node.
---
+++ ft_lstmap.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mchowdhu <mchowdhu@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/22 00:52:30 by mchowdhu          #+#    #+#             */
+/*   Updated: 2026/04/22 00:52:30 by mchowdhu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Iterates through the list ’lst’, applies the
+// function ’f’ to each node’s content, and creates
+// a new list resulting of the successive applications
+// of the function ’f’. The ’del’ function is used to
+// delete the content of a node if needed.
